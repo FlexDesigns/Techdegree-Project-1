914236c84fac81998bd69767a791a15d3b58612e
Thomas Bowser <36521636+FlexDesigns@users.noreply.github.com>
2018-03-05 06:04:41 +0000

    added trivia to the quotes && removed redundant variables

--- js/quotearray.js
+++ js/quotearray.js
@@ -4,60 +4,70 @@ var quotes = [
     quote:"Frankly, my dear, I don't give a damn.",
     source: "Gone with the wind",
     citation: " Rhett Butler (Gable)",
-    year: 1939
+    year: 1939,
+    trivia: "This was the first colour film to win the Academy award for best picture."
   },
   {
     quote:"I'm gonna make him an offer he can't refuse.",
     source: "The Godfather",
     citation: " Michael Corleone (Al Pacino)",
-    year: 1972
+    year: 1972,
+    trivia: "Marlon Brando did not memorize most of his lines and read from cue cards during most of the film."
   },
   {
     quote:"You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
     source: "On the waterfront",
     citation: " Terry Malloy",
-    // year: 1954
+    // year: 1954,
+    trivia: "Shortly after the film's debut in 1954, the AFL-CIO expelled the East Coast longshoremen's union because it was still run by the mob."
   },
   {
     quote:"Toto, I've a feeling we're not in Kansas anymore.",
     source: "The Wizard of Oz",
     // citation: " Judy Garland (Dorothy)",
-    year: 1939
+    year: 1939,
+    trivia:"Many of The Wicked Witch of the West's scenes were either trimmed or deleted entirely, as Margaret Hamilton's performance was thought to be too frightening for audiences."
   },
   {
     quote:"Here's looking at you, kid.",
     source: "Casablanca",
     citation: " Humphrey Bogart (Rick Blaine)",
-    year: 1942
+    year: 1942,
+    trivia:"Rick's Cafe was one of the few original sets built for the film, the rest were all recycled from other Warner Brothers productions due to wartime restrictions on building supplies."
   },
   {
     quote:"Go ahead, make my day.",
     source: "Dirty Harry",
     citation: " Clint Eastwood (Harry Callahan)",
-    year: 1983
+    year: 1983,
+    trivia:"After the film was released, actor Andrew Robinson received several death threats, and had to get an unlisted phone number."
   },
   {
     quote:"May the Force be with you.",
     source: "Star Wars Saga",
     citation: " Various Characters",
-    year: 1977
+    year: 1977,
+    trivia:"In Episode IV: A New Hope, Harrison Ford didn't learn his lines for the intercom conversation in the cell block so that it would sound spontaneous."
   },
   {
     quote:"I love the smell of napalm in the morning...",
     source: "Apocalypse Now",
     // citation: " Robert Duvall (Lt. Bill Kilgore)",
-    // year: 1979
+    // year: 1979,
+    trivia:"Robert Duvall's iconic Oscar-nominated performance as Colonel Kilgore amounts to just eleven minutes of screentime."
   },
   {
     quote:"A census taker once tried to test me. I ate his liver with some fava beans and a nice Chianti.",
     source: "The Silence of the Lambs",
     citation: " Anthony Hopkins (Hannibal Lecter)",
-    year: 1991
+    year: 1991,
+    trivia:"In preparation for his role, Sir Anthony Hopkins studied files of serial killers. Also, he visited prisons, and studied convicted murderers, and was present during some court hearings concerning gruesome murderers and serial killings."
   },
   {
     quote:"You talking to me?",
     source: "Taxi Driver",
     citation: "Robert De Niro (Travis Bickle), Taxi Driver",
-    year: 1976
+    year: 1976,
+    trivia:"Robert De Niro worked fifteen hour days for a month driving cabs as preparation for this role. He also studied mental illness."
   }
 ];
--- js/quotegenerator.js
+++ js/quotegenerator.js
@@ -1,6 +1,6 @@
 //event listener to respond to the window loading, including all scripts. When this is done, it will run the printQuote function every 20 seconds.
 //also runs the script initially on load.
-window.onLoad = printQuote(), setInterval(printQuote, 20000);
+window.onload = printQuote(), setInterval(printQuote, 20000);
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
@@ -8,8 +8,6 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 var citationPresent = true; //boolean for checking if citation is listed
 var yearKnown = true; //boolean for checking if year is listed
-var isRunCheck =0;
-var isRunning = 0;
 
 function getRandomQuote(){ //Function to retrieve quote from the array
   let randomQuote = Math.floor(Math.random()*quotes.length); //Stores a random object value from the array "quotes", but not a specific property. E.g. if it grabs 7, it will grab the 7th object, but not any property of it.
@@ -27,7 +25,9 @@ function printQuote(){
   let sourcetext = quotes[ran].source;
   let citationtext = quotes[ran].citation;
   let year = quotes[ran].year;
+  let trivia = quotes[ran].trivia;
 
+  let triv = document.getElementById('trivia');
   let quo = document.getElementById('quote-box'); //retrieves the div element called 'quote-box'
   let ptags = document.getElementsByTagName('p'); //retrieves all p tag elements and stores them in an array
   let spantags = document.getElementsByTagName('span'); //retrieves all span elements and stores them in an array
@@ -38,36 +38,38 @@ function printQuote(){
   else{
     citationPresent = true;
   }
-  if(quotes[ran].year == null){ //checks if year is present and sets the vaariable to false if its missing, else sets it to true.
+  if(quotes[ran].year == null){ //checks if year is present and sets the variable to false if its missing, else sets it to true.
       yearKnown = false;
   }
   else{
     yearKnown = true;
   }
-  if(yearKnown == false && citationPresent == false){
-    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "</p>";
+  if(yearKnown == false && citationPresent == false){ //Checks if yearKnown is false and citationPresent is false and prints the string without either if both are false.
+    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "</p>" + "<p>"+ trivia + "</p>";
   }
-  else if (yearKnown == true && citationPresent == true){
-    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + citationtext + "</span>" + "<span>" + year + "</span>" + "</p>";
+  else if (yearKnown == true && citationPresent == true){ //Checks if yearKnown is true and citationPresent is true and prints the string with both if they are.
+    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + citationtext + "</span>" + "<span>" + year + "</span>" + "</p>" +"<p>"+ trivia + "</p>";
     spantags[0].classList.add("citation");
     spantags[1].classList.add("year");
   }
-  if(yearKnown == false && citationPresent == true){
-    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + citationtext + "</span>" + "</p>";
+  if(yearKnown == false && citationPresent == true){//Checks if yearKnown is false and citationPresent is true and prints the string without yearKnown if it is false.
+    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + citationtext + "</span>" + "</p>" + "<p>"+ trivia + "</p>";
     spantags[0].classList.add("citation");
   }
-  if(yearKnown == true && citationPresent == false){
-    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + year + "</span>" + "</p>";
+  if(yearKnown == true && citationPresent == false){//Checks if yearKnown is true and citationPresent is false and prints the string without citation if it is false.
+    quo.innerHTML = "<p>" + quotext + "</p>" + "<p>" + sourcetext + "<span>" + year + "</span>" + "</p>" + "<p>"+ trivia + "</p>";
     spantags[0].classList.add("year");
   }
 
-  ptags[0].classList.add("quote");
-  ptags[1].classList.add("source");
+  ptags[0].classList.add("quote"); //adds the quote class to the first set of p tags in the string
+  ptags[1].classList.add("source");//adds the source class to the second set of p tags in the string
+  ptags[2].setAttribute('id','trivia');//adds the trivia id to the third set of p tags in the string, I opted to use this over element.id just simply to ensure it works.
 
+  //Generates random number between 0 and 256 for red, green and blue variables
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
 
-  let bgColour = "rgb(" + red + "," + green + "," + blue + ")";
-  document.body.style.backgroundColor = bgColour;
+  let bgColour = "rgb(" + red + "," + green + "," + blue + ")"; //constructs a string using the above variables which prints as rgb(red,green,blue) and stores it in a variable
+  document.body.style.backgroundColor = bgColour; //sets the background color of the body using CSS styles to the string stored in bgColour
 };
